2f52cad2afbb49a193b37b0205f948b2a6b21109
Wei Xue <wxue@yelp.com>
2021-05-20 11:22:47 -0700

    Added required field to FieldNode

--- src/language/ast.ts
+++ src/language/ast.ts
@@ -538,6 +538,7 @@ export type FieldNode = {
   readonly arguments?: ReadonlyArray<ArgumentNode>;
   readonly directives?: ReadonlyArray<DirectiveNode>;
   readonly selectionSet?: SelectionSetNode;
+  readonly required?: boolean;
 };
 
 export type ArgumentNode = {
